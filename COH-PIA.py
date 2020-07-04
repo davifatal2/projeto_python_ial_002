b129e8fc0ea5f78abf83c73c502252d3f6d95f2d
Vanderxavierb <67658973+Vanderxavierb@users.noreply.github.com>
2020-07-04 18:10:21 -0300

    Update COH-PIA.py

--- COH-PIA.py
+++ COH-PIA.py
@@ -33,6 +33,8 @@ def separa_sentencas(texto):
         del sentencas[-1]
     return sentencas
 
+####edição python
+
 def separa_frases(sentenca):
     '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'[,:;]+', sentenca)
